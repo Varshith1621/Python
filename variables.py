8c4fe4e85f469dba50e34b12677b9439c5f9094d
Varshith <180854390+Varshith1621@users.noreply.github.com.>
2026-06-19 20:42:52 +0530

    Day 2: Variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+a = 5  # Assigning an integer value to the variable a
+b = "Hello"  # Assigning a string value to the variable b
+# Variable names can contain letters (a-z, A-Z), numbers (0-9), and underscores (_).
+# Variable names must start with a letter or an underscore.
+# Variable names are case-sensitive (Name and name are different).
+age = 21
+name = "Varshith"
+is_student = True
+
+
+# Assigning 
+x, y, z = 10, 20, 30
+print(x)  # Output: 10
+print(y)  # Output: 20
+print(z)  # Output: 30
+# x = y = z = 100 print(x, y, z)  # Output: 100 100 100
+
+
+# Variable reassignment
+p = 5
+print(p)  # Output: 5
+p = 10
+print(p)  # Output: 10
